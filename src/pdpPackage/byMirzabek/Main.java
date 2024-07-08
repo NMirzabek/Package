bbb75ff9fb586a87b62bac33c53c7cd58e7af288
Mirzabek Normakhmatov <mirzabeknormakhmatov03@gmail.com>
2024-07-08 09:08:44 +0500

    Created a new branch and Added other three method

--- src/pdpPackage/byMirzabek/Main.java
+++ src/pdpPackage/byMirzabek/Main.java
@@ -1,5 +1,7 @@
 package pdpPackage.byMirzabek;
 
+import java.util.BitSet;
+
 public class Main {
     public static void main(String[] args) {
         Bat bat = new Bat(2, "Black", "fly", 20, "Night");
@@ -7,6 +9,10 @@ public class Main {
         Cat cat = new Cat(4, "Tabby", "Basir", 30);
         Dog dog = new Dog(4, "Black", "Chapa", 10);
         Straus straus = new Straus(2, "Black", "Walks", 10, 900);
+
+        Animal animal = new Animal(4,"Black");
+        Pet pet = new Pet(4, "White", "CatDog");
+        Bird bird = new Bird(2, "Brown", "Fly", 1.5);
         //all of them are objects that are using the proper
         // ties of the animal;
 
